8c8ada958c564a881400105be81d8ef1500d96fb
sim1000 <sim19956@gmail.com>
2023-04-28 23:13:14 +0300

    Add app subtraction

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -2,26 +2,29 @@ package Calculator;
 
 public class Main {
 
-    public class Main {
-
-        public static void main(String[] args) {
-            System.out.println("My app for Users!");
-            System.out.println("Version 1.0");
-            int a = 8;
-            int b = 2;
-            int c = add(a, b);
-            System.out.println(c);
-            int d = multiply(a, b);
-            System.out.println(d);
-        }
-
-        private static int add(int a, int b) {
-            return a + b;
-        }
-
-        private static int multiply(int a, int b) {
-            return a * b;
-        }
+    public static void main(String[] args) {
+        System.out.println("Calculator for base calculations");
+        System.out.println("Version 1.0");
+        int a = 8;
+        int b = 2;
+        int c = add(a, b);
+        System.out.println(c);
+        int d = multiply(a, b);
+        System.out.println(d);
+        int f = subtraction(a, d);
+        System.out.println(f);
     }
 
+    private static int add(int a, int b) {
+        return a + b;
+    }
+
+    private static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    private static int subtraction(int a, int b) {
+        return a - b;
+    }
+}
 
