4e578b88c7878b9a3061ea4ba1944ef3f2caaa5a
sim1000 <sim19956@gmail.com>
2023-04-28 22:52:31 +0300

    Add app version

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -2,23 +2,26 @@ package Calculator;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("My app for Users!");
+    public class Main {
 
-        int a = 8;
-        int b = 2;
-        int c = add(a, b);
-        System.out.println(c);
-        int d = multiply(a, b);
-        System.out.println(d);
+        public static void main(String[] args) {
+            System.out.println("My app for Users!");
+            System.out.println("Version 1.0");
+            int a = 8;
+            int b = 2;
+            int c = add(a, b);
+            System.out.println(c);
+            int d = multiply(a, b);
+            System.out.println(d);
+        }
+
+        private static int add(int a, int b) {
+            return a + b;
+        }
+
+        private static int multiply(int a, int b) {
+            return a * b;
+        }
     }
 
-    private static int add(int a, int b) {
-        return a + b;
-    }
-
-    private static int multiply(int a, int b) {
-        return a * b;
-    }
-}
 
