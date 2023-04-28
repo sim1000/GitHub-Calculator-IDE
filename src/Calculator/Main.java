664f3b5d2d4d6b72ef4364e5d0f6a91c9cf04f33
sim1000 <sim19956@gmail.com>
2023-04-29 00:03:19 +0300

    Add Class Variable, transfer of variables

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -5,32 +5,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Calculator for base calculations");
         System.out.println("Version 1.0");
-        int a = 8;
-        int b = 2;
-        int c = add(a, b);
-        System.out.println(c);
-        int d = multiply(a, b);
-        System.out.println(d);
-        int f = subtraction(a, d);
-        System.out.println(f);
-        int g = divide(a, b);
-        System.out.println(g);
-    }
+        Variable variable = new Variable();
 
-
-    private static int add(int a, int b) {
-        return a + b;
-    }
-
-    private static int multiply(int a, int b) {
-        return a * b;
-    }
-
-    private static int subtraction(int a, int b) {
-        return a - b;
-    }
-    private static int divide(int a, int b){
-        return a / b;
+        System.out.println(variable.a + variable.b);
+        System.out.println(variable.a - variable.b);
+        System.out.println(variable.a / variable.b);
+        System.out.println(variable.a * variable.b);
     }
 }
+
 
---
+++ src/Calculator/Variable.java
@@ -0,0 +1,8 @@
+package Calculator;
+
+public class Variable {
+
+        int a = 8;
+        int b = 2;
+    }
+
